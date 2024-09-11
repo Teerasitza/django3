881666fe56427e786ec7f11838bc07c114929b0c
Teerasitza <6629011035@cdti.ac.th>
2024-09-11 12:30:39 +0700

    first commit

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello to thailand")
+
+def about(request):
+    return HttpResponse("เกี่ยวกับเรา")   
